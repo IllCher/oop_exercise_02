9dd2b777e800cb0e65c5843443881af434d7e121
IllCher <50427226+IllCher@users.noreply.github.com>
2019-09-26 23:34:33 +0300

    added new report and som changes of ; in code

--- oop_lab1/FazzyNumber.cpp
+++ oop_lab1/FazzyNumber.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 #include <cassert>
 #include "FazzyNumber.hpp"
-fn::fn():array{0,0} {};
-fn::fn(double a, double b): array{a, b} {};
+fn::fn():array{0,0} {}
+fn::fn(double a, double b): array{a, b} {}
 void fn::print_value() {
     std::cout<<array[0]<<" "<<array[1]<<std::endl;
-};
+}
 fn fn::sum(const fn &a, const fn &b) {
     fn result;
     result.array[0] = a.array[0] + b.array[0];
     result.array[1] = a.array[1] + b.array[1];
     return result;
-};
+}
 fn fn::difference(const fn &a, const fn &b) {
     fn result;
     result.array[0] = a.array[0] - b.array[1];
     result.array[1] = a.array[1] - b.array[0];
     return result;
-};
+}
 fn fn::comp(const fn &a, const fn &b) {
     fn result;
     result.array[0] = a.array[0] * b.array[0];
     result.array[1] = a.array[1] * b.array[1];
     return result;
-};
+}
 fn fn::division(const fn &a, const fn &b) {
     fn result;
     assert(b.array[0] * b.array[1]);
