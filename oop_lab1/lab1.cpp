b15b27beda3586ef60dc7b28dc98df2f320905ed
IllCher <50427226+IllCher@users.noreply.github.com>
2019-09-27 14:19:01 +0300

    new

--- oop_lab1/lab1.cpp
+++ oop_lab1/lab1.cpp
@@ -5,8 +5,8 @@
 
 int main() {
     double l1,r1,l2,r2;
-    std::ifstream fin("test_04.txt");
-    fin >> l1 >> r1 >> l2 >> r2;
+    //std::ifstream fin("test_04.txt");
+    std::cin >> l1 >> r1 >> l2 >> r2;
     fn a{l1,r1};
     fn b{l2,r2};
     fn::sum(a,b).print_value();
@@ -15,6 +15,6 @@ int main() {
     fn::division(a,b).print_value();
     fn::inv(a).print_value();
     fn::comparison(a, b);
-    fin.close();
+    //fin.close();
     return 0;
 }
